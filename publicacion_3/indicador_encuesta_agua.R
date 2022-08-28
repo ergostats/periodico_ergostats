1b68f078cee41610801c1fe56f8f25bcbecda2b2
Alex Bajaña <alexb.inf.4891@gmail.com>
2022-08-27 19:32:52 -0500

    Corrección script con comentarios, acceso al agua

--- publicacion_3/indicador_encuesta_agua.R
+++ publicacion_3/indicador_encuesta_agua.R
@@ -1,32 +1,35 @@
        
-       ##################################################
-       #                                                #
-       #   ANÁLISIS DE INDICADORES DE CALIDAD DE AGUA   #  
-       #                                                #
-       ##################################################
+
+# ------------------------------------------------------------------------- #
+#                ANÁLISIS DE INDICADORES DE CALIDAD DE AGUA                 #
+# ------------------------------------------------------------------------- #
 
        
-# Instalación de librerías ------------------------------------------------
+# Cargando las librerias ------------------------------------------------
        
 library(haven)
 library(tidyverse)
+library(pins)
 
+# Carpeta compartida RAR --------------------------------------------------
 
+path <- "C:/Users/Alex/OneDrive/Documentos/RAR"
+
+carpeta <- board_folder(path)
 
 # Lectura de la base de datos ---------------------------------------------
 
-tabla_agua <- read_dta("publicacion_1/agua_ash2019.dta")
+tabla_agua <- read_dta("../nuevos_hospitales/calidad_agua/bdd_ash2019.dta")
 
 
-# Visualización de los datos ----------------------------------------------
+# Primer vistazo a la estructura de los datos ------------------------------
 
 glimpse(tabla_agua)
 
 
-# Verificar el tipo de datos ----------------------------------------------
+# Revisando que calse de objeto es nuestra tabla de datos ------------------
 
 class(tabla_agua)
-
 
 
 # Mostrar los atributos de las variables ----------------------------------
@@ -35,11 +38,9 @@ diccionario <- map(.x = tabla_agua,
                    .f = attributes)
 
 
-# Información sobre diccionario -------------------------------------------
+# Estructura de la lista de atributos que componen nuestro diccionario ----
 
 str(diccionario)
-
-
 
 # Etiquetas de las variables ----------------------------------------------
 
@@ -69,43 +70,97 @@ diccionario_categorias <- map(.x = diccionario,
 # Escribir archivos rds con un comentario úti -----------------------------
 
 write_rds(x = diccionario_variables, file = "diccionario_variables_agua.rds" )
+
 write_rds(x = diccionario_categorias, file = "diccionario_categorias_agua.rds")
-
-
 
 # Construcción de los indicadores -----------------------------------------
 
 
 # Indicador 6.1 -----------------------------------------------------------
+# Porcentaje de hogares que utiliza suministros seguros de agua para beber
 
-indicador <- select(tabla_agua, vi16, vi17, vi26, ra01e_f, ra03_f, ra04_f, ra022_f, ra023_f, vi17a, vi17b, vi17c)
 
-indicador <- 
+
+indicador_6_1 <- 
   indicador %>% 
-  mutate(tuberia = if_else(condition = vi16 <=3,true = 1, false = 0),
-         agua_cp1 = if_else(condition = vi17 <=6 & vi17 >=5 & tuberia==1, true = 1, false = 0),
-         validtest_f = if_else (condition = ra01e_f >=24 & ra01e_f <=48 & ra03_f==2 & ra04_f==2,true = 1, false = 0),
-         coli_f = if_else(condition = validtest_f==1 & ra022_f==1, true = 1, false = 0),
-         agua_cp2_f = if_else(condition = validtest_f==1 & (ra023_f==2 | ra022_f==2), true = 1, false =  0),
-         agua_cp3 = if_else(condition = vi17a >=1 & vi17a <=2 & vi17 >=5 & vi17 <=6, true = 1, false = if_else(condition = vi17a==3 & vi17b <=30, true =  2, false = 3 )),
-         agua_cp4 = if_else(condition = vi17c ==1, true = 1, false = if_else(condition = vi17c>=1 & vi17c <=2, true =  2, false = 3 )))
+  select( vi16, vi17, vi26, 
+          ra01e_f, ra03_f, ra04_f, ra022_f,
+          ra023_f, vi17a, vi17b, vi17c) %>% 
+  mutate(
+    # Tipo de suministro
+    
+    tuberia = if_else(condition = vi16 <= 3,
+                           true = 1, 
+                           false = 2),
+         agua_cp1 = case_when(vi17 %in% c(1:3,7,9)   ~ 1,
+                              vi17 %in% c(4,8,10,12) ~ 2,
+                              vi17 %in% c(11,13)     ~ 3,
+                              vi17 %in% c(6,5) & tuberia==1 ~ 1,
+                              vi17 %in% c(6,5) & tuberia==2 ~ 2
+                              ),
+         
+         
+         # Se identifican pruebas válidas fuente
+         
+         validtest_f = case_when(vi26 == 1 & !is.na(ra01e_f) ~ 0
+                                 between(ra01e_f,left = 24,right = 48) & ra03_f == 2 & ra04_f == 2 ~ 1,
+                                 vi26 == 2 ~ 2),
+    
+    # Presencia de coliformes fuente
+    
+         coli_f = case_when(
+           validtest_f == 1 & ra023_f == 1 ~ 1,
+           validtest_f == 1 & ra023_f == 2 ~ 2,
+           validtest_f == 1 ~ 0),
+    
+    # Ausencia de E. coli fuente
+    
+         agua_cp2_f = case_when(
+           validtest_f == 1 & (ra023_f == 2 | ra022_f == 2) ~ 1,
+           validtest_f == 1 & ra023_f == 1  ~ 2,
+           validtest_f == 1 & ra023_f == 1  ~ 0),
+    
+    # Cercanía del suministro
+    # Supuesto: agua embotellada cerca para el consumo
+    # Se  encuentra fuera de la vivienda, edifico/lote y no sabe a cuanto se encuentra (en minutos) la fuente 
+         agua_cp3 = case_when(
+           vi17a %in% c(1, 2,5,6) ~ 1, 
+           vi17a == 3 & vi17b <= 30 ~ 2,
+           vi17a == 3 & vi17b > 30 ~ 3,
+           vi17b == 999 & (vi17a != 1 | vi17a != 2) ~ NA_real_),
+    
+         agua_cp4 = case_when(vi17c == 1 ~ 1,
+                              vi17c == 3 ~ NA_integer_,
+                              vi17c > 1 ~ 2))
 
 
-#Creación de un dataframe más conciso
+# Corrección indicador de agua: -------------------------------------------
 
-i_agua <- select(indicador, agua_cp1, agua_cp2_f, agua_cp3, agua_cp4)
-
-# manejo seguro
-i_gua <- 
-  i_agua %>% 
-  mutate(manejo_seguro = if_else(condition = agua_cp1==1 & agua_cp2_f==1 & agua_cp3==1 & agua_cp4==1, true = 1, false = 0),
-         básico_1 = if_else(condition = agua_cp1==1 & agua_cp2_f==1 & agua_cp3==1 & agua_cp4==2, true = 1, false = 0),
-         basico_1 = if_else(condition = agua_cp1==1 & agua_cp2_f==1 & agua_cp3==2, true = 1, false = 0),
-         basico_2 = if_else(condition = agua_cp1==1 & agua_cp2_f==2 & agua_cp3==1, true = 1, false = 0),
-         basico_2 = if_else(condition = agua_cp1==1 & agua_cp2_f==2 & agua_cp3==2, true = 1, false = 0),
-         limitado = if_else(condition = agua_cp1==1 & agua_cp3==3, true = 1, false = 0),
-         no_mejorado = if_else(condition = agua_cp1 == 2, true = 1, false = 0),
-         superficial = if_else(condition = agua_cp1==3, true = 1, false = 0))
+# %>% 
+  mutate(manejo_seguro = if_else(condition = agua_cp1 == 1 & agua_cp2_f == 1 & agua_cp3 == 1 & agua_cp4 == 1, 
+                                 true = 1, 
+                                 false = 0),
+         básico_1 = if_else(condition = agua_cp1 == 1 & agua_cp2_f == 1 & agua_cp3 == 1 & agua_cp4 == 2, 
+                            true = 1, 
+                            false = 0),
+         basico_1 = if_else(condition = agua_cp1 == 1 & agua_cp2_f == 1 & agua_cp3 == 2, 
+                            true = 1, 
+                            false = 0),
+         basico_2 = if_else(condition = agua_cp1 == 1 & agua_cp2_f == 2 & agua_cp3 == 1,
+                            true = 1,
+                            false = 0),
+         basico_2 = if_else(condition = agua_cp1 == 1 & agua_cp2_f == 2 & agua_cp3 == 2, 
+                            true = 1, 
+                            false = 0),
+         limitado = if_else(condition = agua_cp1 == 1 & agua_cp3 == 3, 
+                            true = 1, 
+                            false = 0),
+         no_mejorado = if_else(condition = agua_cp1  ==  2, 
+                               true = 1, 
+                               false = 0),
+         superficial = if_else(condition = agua_cp1 == 3, 
+                               true = 1, 
+                               false = 0))
 
 
 
